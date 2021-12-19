0f33b2c15eb4255f2bd507cce98b19c4934a8c95
Valentina Bearzotti <valebearzotti1@gmail.com>
2021-12-19 17:01:50 -0300

    work in progress upload

---
+++ src/styles/Home.styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+import { createGlobalStyle } from "styled-components"
+
+export const Container = styled.div`
+
+`
+
+export const GlobalStyle = createGlobalStyle`
+    body{
+        margin: 0;
+        padding: 0;
+        background-color: #fff;
+        overflow-y: hidden;
+        overflow-x: hidden;
+    }
+`
