6995e6355caab523077d9e7ce0bec5a6f9106b87
Valentina Bearzotti <valebearzotti1@gmail.com>
2021-12-19 19:00:39 -0300

    navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import Hero from '../components/Hero'
-import { Container, GlobalStyle, Navbar, Logo } from '../styles/Home.styled'
+import { Container, GlobalStyle, Navbar, Logo, Proyectos, Nosotros } from '../styles/Home.styled'
 import logo from '../assets/logo.png'
+import izq from '../assets/proyectos.svg'
+import der from '../assets/nosotros.svg'
 
 function Home() {
 
     return (
         <Container>
             <GlobalStyle />
-            <Hero />
+            {/* <Hero /> */}
             <Navbar>
+                <Proyectos>
+                    <img src={izq}/>
+                    Proyectos
+                </Proyectos>
                 <Logo>
                     <img src={logo} />
                 </Logo>
+                <Nosotros>
+                    Nosotros
+                    <img src={der}/>
+                </Nosotros>
             </Navbar>
 
         </Container>
--- src/styles/Home.styled.js
+++ src/styles/Home.styled.js
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 import { createGlobalStyle } from "styled-components"
+import './styles.css'
 
 export const Container = styled.div`
 
@@ -10,6 +11,7 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         background-color: #fff;
+        font-family: 'Nunito', sans-serif;
     }
 `
 
@@ -22,6 +24,28 @@ export const Logo = styled.div`
     margin-right: auto;
     img {
         width: 100%;
+        height: auto;
+    }
+`
+
+export const Proyectos = styled.div`
+    margin-right: auto;
+    cursor: pointer;
+    padding: 1em;
+    img {
+        margin-right: 1em;
+        width: 50px;
+        height: auto;
+    }
+`
+
+export const Nosotros = styled.div`
+    margin-left: auto;
+    cursor: pointer;
+    padding: 1em;
+    img {
+        margin-left: 1em;
+        width: 50px;
         height: auto;
     }
 `
